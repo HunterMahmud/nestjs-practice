526868c3881d23e4a1b40e3326116be6414099c8
huntermahmud <mahmudthedatahunter@gmail.com>
2025-02-17 10:56:25 +0600

    add: get the ip protocol host url method & date time with log

--- src/ShopModule/shop.middleware.ts
+++ src/ShopModule/shop.middleware.ts
@@ -3,8 +3,15 @@ import { Response, Request, NextFunction } from 'express';
 
 @Injectable()
 export class ShopMiddleware implements NestMiddleware {
-    use(_req: Request, _res:Response, next:NextFunction){
+    use(req: Request, _res:Response, next:NextFunction){
         console.log("shop middlewares")
+        const host = req.get("host");
+        const url = req.originalUrl;
+        const method = req.method;
+        const date = new Date();
+        const ip = req.ip;
+
+        console.log(`IP:${ip} ${req.protocol}://${host}${url} METHOD: ${method} ${date.toTimeString()}`) // loggin with the req with time date host method and ip address
         next()
     }
 }
