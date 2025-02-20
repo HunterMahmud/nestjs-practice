c1a18528e9c2b9a7449d7d3b64f294c1585594e2
huntermahmud <mahmudthedatahunter@gmail.com>
2025-02-20 15:27:15 +0600

    making api documentation using swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { RootModule } from './root.module';
 import { NumberOfRequest } from './Middlewares/global.middlewares';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 
@@ -11,7 +12,23 @@ async function bootstrap() {
    * setting up the global middlware for counting the number of request
    */
 
+  
+
   app.use(NumberOfRequest);
+
+  /**
+   * @OpenAPI -- documentation using swagger
+   */
+
+  const config = new DocumentBuilder()
+  .setTitle('User example api')
+  .setDescription('The users API description')
+  .setVersion('1.0')
+  .addTag('users')
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000, ()=>{
     console.log(`Server is running on port 3000`)
